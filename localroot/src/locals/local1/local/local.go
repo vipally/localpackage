4822e942418bfb71b13aed58727fbc76dcb07190
vipally <vipally@gmail.com>
2017-11-22 11:16:52 +0800

    test case

--- localroot/src/locals/local1/local/local.go
+++ localroot/src/locals/local1/local/local.go
@@ -18,7 +18,7 @@ func GetGopath() string {
 	if gGoPath == "" {
 		s := os.Getenv("GOPATH")
 		if ss := strings.Split(s, ";"); ss != nil && len(ss) > 0 {
-			gGoPath = formatPath(ss[0]) + "/src"
+			gGoPath = filepath.Join(ss[0], "src")
 		}
 	}
 	return gGoPath
