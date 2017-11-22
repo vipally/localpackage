afb95092cb1baf23f7a3eb02ccf1952728cb2067
vipally <vipally@gmail.com>
2017-11-22 12:22:26 +0800

    test case

--- localroot/src/witherrors/main.go
+++ localroot/src/witherrors/main.go
@@ -1,8 +1,9 @@
-package main
+package main //import "#"
 
 import (
 	//main.go:5:2: cannot import local-package E:\dev\oversea_dev\oversea-code\trunk\gocode\src\github.com\vipally\localpackage\projectroot\src\locals\local2 from global style
-	"github.com/vipally/localpackage/projectroot/src/locals/local2"
+	"github.com/vipally/localpackage/localroot/src/locals/local2"
+	//"#/locals/local2"
 )
 
 func main() {
