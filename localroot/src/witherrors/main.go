17819b799bee407814e5ec6f08eff95c3bbae044
vipally <vipally@gmail.com>
2017-11-22 17:41:51 +0800

    test code

--- localroot/src/witherrors/main.go
+++ localroot/src/witherrors/main.go
@@ -1,7 +1,7 @@
 package main //import "#"
 
 import (
-	//main.go:5:2: cannot import local-package E:\dev\oversea_dev\oversea-code\trunk\gocode\src\github.com\vipally\localpackage\projectroot\src\locals\local2 from global style
+	//main.go:5:2: cannot import local-package E:\gocode\src\github.com\vipally\localpackage\localroot\src\locals\local2 from global style
 	"github.com/vipally/localpackage/localroot/src/locals/local2"
 	//"#/locals/local2"
 )
