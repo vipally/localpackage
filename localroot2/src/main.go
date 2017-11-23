501a77dd340e79f461de21000e2238b097ec433e
vipally <vipally@gmail.com>
2017-11-23 16:52:02 +0800

    localroot2

---
+++ localroot2/src/main.go
@@ -0,0 +1,19 @@
+package main//import "#"
+import(
+	"#/local"
+	"github.com/vipally/localpackage/projectroot/src/locals/local2"
+)
+func main() {
+	fmt.Println("main    import [#/local]")
+	thisFilePath := local.GetThisFilepath(2)
+	fmt.Printf("Gopath is      : %s\n", local.GetGopath())
+	fmt.Printf("ProjectRoot is : %s\n", local.GetProjectRootpath(2))
+	fmt.Printf("ThisPackagePath: %s\n", local.GetThisFilepath(2))
+	fmt.Printf("ReleatGopath is: %s\n", local.GetReleatGopath(thisFilePath))
+
+	fmt.Println("**********************************************************")
+	fmt.Println("main    import [github.com/vipally/localpackage/projectroot/src/locals/local2]")
+	local2.PackageMain()
+	fmt.Println("**********************************************************")
+	
+}
