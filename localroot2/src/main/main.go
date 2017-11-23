3c55aa37bed5ab31b19b79fbe09e87ddaad66397
Ally Dale <vipally@gmail.com>
2017-11-24 00:17:35 +0800

    test case

--- localroot2/src/main/main.go
+++ localroot2/src/main/main.go
@@ -1,20 +1,23 @@
 package main
+
 //import "#"
-import(
-	"#/local"
-	"github.com/vipally/localpackage/localroot/src/locals/local2"
+import (
+	//"#/local"
+	//"github.com/vipally/localpackage/localroot/src/locals/local2"
+	"fmt"
 )
+
 func main() {
-	fmt.Println("main    import [#/local]")
-	thisFilePath := local.GetThisFilepath(2)
-	fmt.Printf("Gopath is      : %s\n", local.GetGopath())
-	fmt.Printf("ProjectRoot is : %s\n", local.GetProjectRootpath(2))
-	fmt.Printf("ThisPackagePath: %s\n", local.GetThisFilepath(2))
-	fmt.Printf("ReleatGopath is: %s\n", local.GetReleatGopath(thisFilePath))
+	//		fmt.Println("main    import [#/local]")
+	//		thisFilePath := local.GetThisFilepath(2)
+	//		fmt.Printf("Gopath is      : %s\n", local.GetGopath())
+	//		fmt.Printf("ProjectRoot is : %s\n", local.GetProjectRootpath(2))
+	//		fmt.Printf("ThisPackagePath: %s\n", local.GetThisFilepath(2))
+	//		fmt.Printf("ReleatGopath is: %s\n", local.GetReleatGopath(thisFilePath))
 
 	fmt.Println("**********************************************************")
 	fmt.Println("main    import [github.com/vipally/localpackage/projectroot/src/locals/local2]")
-	local2.PackageMain()
+	//local2.PackageMain()
 	fmt.Println("**********************************************************")
-	
+
 }
