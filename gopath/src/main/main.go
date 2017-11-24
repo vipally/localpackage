b92ba793b387ffa6341aa1a1e3938090468b68a5
vipally <vipally@gmail.com>
2017-11-24 18:58:40 +0800

    test case

---
+++ gopath/src/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"local"
+)
+
+func main() {
+	println("Gopath/src/main")
+	fmt.Println("main    import [#/local]")
+	thisFilePath := local.GetThisFilepath(2)
+	fmt.Printf("Gopath is      : %s\n", local.GetGopath())
+	fmt.Printf("ProjectRoot is : %s\n", local.GetProjectRootpath(2))
+	fmt.Printf("ThisPackagePath: %s\n", local.GetThisFilepath(2))
+	fmt.Printf("ReleatGopath is: %s\n", local.GetReleatGopath(thisFilePath))
+}
