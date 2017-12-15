4657a13b3e24af3ed10d5c9411355ffd3249a0b5
Ally Dale <vipally@gmail.com>
2017-12-15 17:26:18 +0800

    test buildtime

--- gopath/src/const/main.go
+++ gopath/src/const/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	cons "const/cons"
+	//cons "const/cons"
 	"runtime"
 	"time"
 )
 
 func main() {
-	println(cons.ConstVal)
 	unix := runtime.BuildTimestamp()
 	t := time.Unix(unix, 0)
-	println(unix, t.String())
+	println("hello world")
+	println("buildtimestamp:", unix)
+	println("buildtime     :", t.String())
+	// Output:
+	// hello world
+	// buildtimestamp: 1513329454
+	// buildtime     : 2017-12-15 17:17:34 +0800 CST
 }
