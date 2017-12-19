cb524f2d2c899c07e6de6fb8cad3344e979dc1da
Ally Dale <vipally@gmail.com>
2017-12-19 15:07:35 +0800

    test runtime.BuildTimestamp

--- gopath/src/const/main.go
+++ gopath/src/const/main.go
@@ -7,13 +7,11 @@ import (
 )
 
 func main() {
-	unix := runtime.BuildTimestamp()
-	t := time.Unix(unix, 0)
-	println("hello world")
-	println("buildtimestamp:", unix)
-	println("buildtime     :", t.String())
+	println("hello world!")
+	println("buildtime:", runtime.BuildTimestamp(), time.BuildTime().String())
+	println("now time :", runtime.UnixNow(), time.Now().String())
 	// Output:
-	// hello world
-	// buildtimestamp: 1513329454
-	// buildtime     : 2017-12-15 17:17:34 +0800 CST
+	//hello world!
+	//buildtime: 1513666403 2017-12-19 14:53:23 +0800 CST
+	//now time : 1513666403 2017-12-19 14:53:23.9419813 +0800 CST m=+0.047002701
 }
