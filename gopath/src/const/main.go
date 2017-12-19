599d22196576dd73c7f111c1d70bee2b11714255
Ally Dale <vipally@gmail.com>
2017-12-19 15:25:57 +0800

    test case

--- gopath/src/const/main.go
+++ gopath/src/const/main.go
@@ -9,9 +9,8 @@ import (
 func main() {
 	println("hello world!")
 	println("buildtime:", runtime.BuildTimestamp(), time.BuildTime().String())
-	println("now time :", runtime.UnixNow(), time.Now().String())
 	// Output:
-	//hello world!
-	//buildtime: 1513666403 2017-12-19 14:53:23 +0800 CST
-	//now time : 1513666403 2017-12-19 14:53:23.9419813 +0800 CST m=+0.047002701
+	// hello world!
+	// buildtime: 1513666403 2017-12-19 14:53:23 +0800 CST
+	// now time : 1513666403 2017-12-19 14:53:23.9419813 +0800 CST m=+0.047002701
 }
